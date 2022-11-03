c8cd49961f15dc87f4b2b043b01c3a46d558bbdf
Domenic <91065796+GIT-DMU@users.noreply.github.com>
2022-11-03 09:08:34 +0100

    Update ExportAutopilotData.ps1

--- ExportAutopilotData.ps1
+++ ExportAutopilotData.ps1
@@ -1,7 +1,6 @@
 <#
 	This script can be used on a Windows To Go - USB/HardDrive.
 	This script will get the required information for MDM to use Autopilot on the script executed Device.
-
 #>
 
 # Get SerialNumber
@@ -11,8 +10,13 @@ $Serialnumber = (Get-CimInstance -Class Win32_BIOS).SerialNumber
 $date = Get-Date -Format yyyyMMdd
 
 # Create Export-Path
-$Path = ($MyInvocation.MyCommand.Path -replace $MyInvocation.MyCommand.Name, "") + "AutoPilot-Export\" + $date + "_Autopilot_$serialnumber.csv"
-
+if(!(Test-Path
+$Path = ($MyInvocation.MyCommand.Path -replace $MyInvocation.MyCommand.Name, "") + "AutoPilot-Export\"  # This will use Path of the ExportAutopilotData.ps1 and add a AutoPilot-Export directory
+if(!(Test-Path $Path) {
+	New-Item -ItemType Directory -Path $Path
+}
+$FileName = $date + "_Autopilot_$serialnumber.csv"
+$FullPath = $Path + $FileName
 # Get HardwareHash (requires Admin permissions)
 $HardwareHash = (Get-CimInstance -Namespace root/CIMV2/mdm/dmmap -ClassName MDM_DevDetail_Ext01).DeviceHardwareData
 
@@ -24,7 +28,7 @@ $output = [PSCustomObject]@{
 } 
 
 # Create the CSV
-$output | Export-Csv -LiteralPath $Path -Delimiter "," -Encoding utf8 -Force -NoTypeInformation
+$output | Export-Csv -LiteralPath $FullPath -Delimiter "," -Encoding utf8 -Force -NoTypeInformation
 
 # Restart the PC
 Restart-Computer -Force
