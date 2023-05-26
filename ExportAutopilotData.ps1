f5487162ca8dae5239d9469207f21d543ea525b6
Domenic <91065796+GIT-DMU@users.noreply.github.com>
2023-05-26 09:29:02 +0200

    Update ExportAutopilotData.ps1
    Safe all exports of the same day to 1x CSV

--- ExportAutopilotData.ps1
+++ ExportAutopilotData.ps1
@@ -1,6 +1,7 @@
 <#
 	This script can be used on a Windows To Go - USB/HardDrive.
 	This script will get the required information for MDM to use Autopilot on the script executed Device.
+    	The data will be safed to a csv in the "AutoPilot-Export" directory, all imports of the same day are located in 1 CSV.
 #>
 
 # Get SerialNumber
@@ -10,25 +11,30 @@ $Serialnumber = (Get-CimInstance -Class Win32_BIOS).SerialNumber
 $date = Get-Date -Format yyyyMMdd
 
 # Create Export-Path
-if(!(Test-Path
 $Path = ($MyInvocation.MyCommand.Path -replace $MyInvocation.MyCommand.Name, "") + "AutoPilot-Export\"  # This will use Path of the ExportAutopilotData.ps1 and add a AutoPilot-Export directory
-if(!(Test-Path $Path) {
-	New-Item -ItemType Directory -Path $Path
+if (!(Test-Path $Path)) {
+    New-Item -ItemType Directory -Path $Path
 }
-$FileName = $date + "_Autopilot_$serialnumber.csv"
+$FileName = $date + "_Autopilot_Export.csv"
 $FullPath = $Path + $FileName
+
 # Get HardwareHash (requires Admin permissions)
 $HardwareHash = (Get-CimInstance -Namespace root/CIMV2/mdm/dmmap -ClassName MDM_DevDetail_Ext01).DeviceHardwareData
 
 # Create an Array which will be Exported to the CSV
 $output = [PSCustomObject]@{
     "Device Serial Number" = $Serialnumber;
-    "Windows Product ID" = "";
-    "Hardware Hash" = $HardwareHash
+    "Windows Product ID"   = "";
+    "Hardware Hash"        = $HardwareHash
 } 
 
-# Create the CSV
-$output | Export-Csv -LiteralPath $FullPath -Delimiter "," -Encoding utf8 -Force -NoTypeInformation
+# Check ob bereits CSV erstellt wurde
+if (!(Test-Path -Path $FullPath)) {
+    # NO: Create the CSV
+    $output | Export-Csv -LiteralPath $FullPath -Delimiter "," -Encoding utf8 -Force -NoTypeInformation
+}
+else {
+    # YES: Append
+    $output | Export-Csv -LiteralPath $FullPath -Delimiter "," -Encoding utf8 -Force -Append
+}
 
-# Restart the PC
-Restart-Computer -Force
